ef7bc6c5c703b5346bbc7fb0f4faa5ccfb0ba8e4
ronggh@sina.cn <ronggh@sina.cn>
2020-09-29 20:57:10 +0800

    Stream

---
+++ stream-consume-6601/src/main/java/cn/alan/StreamConsumeMain.java
@@ -0,0 +1,11 @@
+package cn.alan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamConsumeMain {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamConsumeMain.class, args);
+    }
+}
---
+++ stream-consume-6601/src/main/java/cn/alan/controller/MessageConsumeController.java
@@ -0,0 +1,21 @@
+package cn.alan.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableBinding(Sink.class)
+public class MessageConsumeController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message) {
+        String result = "消费者端口号：" + serverPort + " 接收到的消息：===> " + message.getPayload();
+        System.out.println(result);
+    }
+}
